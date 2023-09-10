7ea172e3f972740badf67dad507ecbfca523b25b
Asus <vaxa2320@gmail.com>
2023-09-10 15:59:27 +0300

    bach011

--- src/main/java/by/IT/constants/ApplicationConstants.java
+++ src/main/java/by/IT/constants/ApplicationConstants.java
@@ -1,10 +1,8 @@
 package by.IT.constants;
-
 public class ApplicationConstants {
   public static final String LOGIN_CONTROLLER = "/login";
   public static final String LOGOUT_CONTROLLER = "/logout";
   public static final String REGISTRATION_CONTROLLER = "/registration";
-
   public static final String USER_NOT_FOUND = "User not found";
   public static final String USER_NOT_REGISTERED= "User not registered";
 }
