d70f7190050a8fc076e08fdd15d9316458aed8ab
Asus <vaxa2320@gmail.com>
2023-09-17 16:01:39 +0300

    bach011

--- src/main/java/by/IT/constants/DbConstants.java
+++ src/main/java/by/IT/constants/DbConstants.java
@@ -6,7 +6,6 @@ public class DbConstants {
     public static final String NAME_COL = "name";
     public static final String PASS_COL = "password";
     public static final String EMAIL_COL = "email";
-
     public static final String SELECT_USER = "SELECT id, name, email FROM user WHERE login = ? AND password = ?";
     public static final String INSERT_USER =" INSERT INTO user (login, name, email, password) VALUES (?,?,?,?)";
     public static final String SELECT_USER_BY_LOGIN = "SELECT id,  FROM user WHERE login = ?";
