6b3f309e81e066278160ace8073b4efd31021490
Asus <vaxa2320@gmail.com>
2023-12-03 17:52:58 +0300

    user1

---
+++ src/main/java/by/IT/model/entities/User.java
@@ -0,0 +1,20 @@
+package by.IT.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+
+public class User {
+    private int id;
+    @NonNull
+    private String login;
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+}
